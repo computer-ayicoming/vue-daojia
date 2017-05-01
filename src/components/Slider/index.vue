786651185a98e0f9b14b3610d744c213a071c4cc
Raoul1996 <baobaozhou199606@gmail.com>
2017-05-01 11:17:58 +0800

    【fix】 house-keeping min-width

--- src/components/Slider/index.vue
+++ src/components/Slider/index.vue
@@ -1,7 +1,6 @@
 <template>
   <el-carousel :interval="5000" arrow="always">
     <el-carousel-item v-for="item in 4" :key="item">
-      <h3>{{ item }}</h3>
     </el-carousel-item>
   </el-carousel>
 </template>
